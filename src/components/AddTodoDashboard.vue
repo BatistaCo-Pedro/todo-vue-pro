8c19495e5bdb670bd15d0785c8ba47deb688a831
Pedro <Pedrocorreiabatista@gmail.com>
2023-03-23 11:14:16 +0100

    lorem ipsum test

--- src/components/AddTodoDashboard.vue
+++ src/components/AddTodoDashboard.vue
@@ -35,9 +35,19 @@ export default {
 
     watch: {
         show_dash() {
-            this.$nextTick(() => {
-                this.$refs.edit_todo_input.focus();
-            })
+          this.$nextTick(() => {
+              this.$refs.edit_todo_input.focus();
+          })
+        },
+        new_description() {
+          if(this.new_description.toLocaleLowerCase() === "li()") {
+            this.new_description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed" +
+            "diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." + 
+            "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata" + 
+            "sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed " + 
+            "diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. " + 
+            "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+          }
         }
     }
 }
